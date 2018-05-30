2a8930d0278932f54e1404ed574a8973143955b3
Sven Sterbling <sterbling@de.ibm.com>
2018-05-30 16:35:49 +0200

    Added general CORS support

--- app.js
+++ app.js
@@ -31,6 +31,13 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS, HEAD");
+  next();
+});
+
 debug('Configuring bots...');
 let initPromise = bots(app)
   .then(config => {
